ab55f09ee1a80e038fc0cb54671dcc18699635d8
El Mehdi ASSALI <el.mehdi.assali@ibm.com>
2022-07-24 02:36:26 +0200

    :sparkles: Feat(publish-problem): Create function and deploy pipeline

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as listProblems } from "./listProblems";
 export { default as subscribeUser } from "./subscribeUser";
+export { default as publishProblem } from "./publishProblem";
---
+++ src/publishProblem.ts
@@ -0,0 +1,39 @@
+import { Storage } from "@google-cloud/storage";
+import {
+  HttpFunction,
+  Request,
+  Response,
+} from "@google-cloud/functions-framework";
+import { setCors } from "./util/cors.helper";
+
+const publishProblem: HttpFunction = async (req: Request, res: Response) => {
+  const shouldReturn = setCors(req, res);
+
+  if (shouldReturn) {
+    return;
+  }
+
+  const storage = new Storage();
+  const bucket = storage.bucket("solveit-problems");
+  const problem = req.body;
+  const file = bucket.file(`${problem.slug}.json`);
+
+  const [fileExists] = await file.exists();
+  if (fileExists) {
+    res
+      .status(400)
+      .json({ message: "A problem with the same name already exists." });
+
+    return;
+  }
+
+  problem.metadata.createdAt = Number(new Date());
+
+  await file.save(JSON.stringify(problem), {
+    contentType: "application/json",
+  });
+
+  res.json({ message: "Problem uploaded." });
+};
+
+export default publishProblem;
